e805cc48d81447d1ef80463288cd7dba9d9d60e8
mxyzpltk <vuadinh69@gmail.com>
2025-11-28 01:19:57 +0700

    add logindto

---
+++ SK.FinalP.Application/DTOs/LoginDto.cs
@@ -0,0 +1,9 @@
+namespace SK.FinalP.Application.DTOs;
+
+public class LoginDto : BaseDto
+{
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
